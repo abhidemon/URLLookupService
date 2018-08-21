10e82b0ca734364207060aead52e3132bea10bb0
princemanohar <princemanohar0909@gmail.com>
2018-08-22 01:29:30 +0530

    code to fetch malware info for a url from db

---
+++ url_info_db.py
@@ -0,0 +1,32 @@
+import MySQLdb
+
+db = MySQLdb.connect("localhost", "root", "root")
+
+def getURLInfo(url):
+    cursor = db.cursor()
+    query = "SELECT malware_info from dev.url_infos where url = %s"
+    cursor.execute(query, (url,))
+
+    result = cursor.fetchone()
+
+    if (not result):
+        raise Exception("Url not found")
+
+    malware_info = result[0]
+    print("Got malware_info : "+malware_info +" , for url : "+url)
+
+    return malware_info
+
+if __name__=="__main__":
+    # Testing the code
+    info = getURLInfo("http://google.com")
+    print (info)
+
+
+
+
+
+
+
+
+
