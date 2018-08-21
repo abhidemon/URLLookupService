83dedecd0350bb626c2a7fe278562b6b652dca4d
princemanohar <princemanohar0909@gmail.com>
2018-08-22 01:56:32 +0530

    Add api for handling request for urlinfo

---
+++ url_info_controller.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template, request, redirect, url_for
+from url_info_db import getURLInfo
+
+app = Flask(__name__)
+
+@app.route( '/urlinfo/1/<hostname_and_port>/<original_path_and_query_string>')
+def get_url_information(hostname_and_port, original_path_and_query_string):
+    malware_info = getURLInfo(hostname_and_port)
+    if not malware_info:
+        malware_info = "NOT_MALWARE"
+    return malware_info
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=9000)
