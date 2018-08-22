77881b1c011beb2d923302207c53b0fa5a9dd40d
princemanohar <princemanohar0909@gmail.com>
2018-08-22 11:32:44 +0530

    Api for saving a new url info

--- app/url_info_controller.py
+++ app/url_info_controller.py
@@ -1,16 +1,23 @@
-from flask import Flask
-
-from app.url_info_db import getURLInfo
+from flask import Flask, request
+from app.url_info_db import get_url_info, save_url_info
+import json
 
 app = Flask(__name__)
 
+
 @app.route( '/urlinfo/1/<hostname_and_port>/<original_path_and_query_string>')
 def get_url_information(hostname_and_port, original_path_and_query_string):
-    malware_info = getURLInfo(hostname_and_port)
+    malware_info = get_url_info(hostname_and_port)
     if not malware_info:
         malware_info = "NOT_MALWARE"
     return malware_info
 
+
+@app.route('/urlinfo', methods=['POST'])
+def save_url_info_api():
+    url = request.form['url']
+    malware_info = request.form['malware_info']
+    return save_url_info(url, malware_info)
 
 if __name__ == '__main__':
     app.run(debug=True, port=9000)
